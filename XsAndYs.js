7e3c506551b0370879a24b5007769897a4331be8
PeterSaan <113171626+PeterSaan@users.noreply.github.com>
2022-10-30 02:43:22 +0300

    Add files via upload

---
+++ XsAndYs.js
@@ -0,0 +1,30 @@
+const letters = ["xxxyyy", "xyxyxyx" , "xxyyxxyxy", "", "x"]
+let lettersInWord = 0
+
+function countXsAndYs(word) { 
+    const letter = letters.forEach(
+        function(){ 
+            if(word !== "") {
+                for(i = 0; i < letters.length; i++) { 
+                    
+                }
+            } else { 
+                return "No letters to count!"
+              }    
+        }
+    )
+
+}
+
+/*function char_count(str, letter) 
+{
+ var letter_Count = 0;
+ for (var position = 0; position < str.length; position++) 
+ {
+    if (str.charAt(position) == letter) 
+      {
+      letter_Count += 1;
+      }
+  }
+  return letter_Count;
+}*/
